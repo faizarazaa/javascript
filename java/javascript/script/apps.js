df828068be2bf3d36c03f7266faee320595b2350
faizarazaa <125462568+faizarazaa@users.noreply.github.com>
2023-04-07 01:26:35 +0500

    Add files via upload

---
+++ java/javascript/script/apps.js
@@ -0,0 +1,176 @@
+// var username = "Mark";
+
+// alert('your name' + username)
+
+// username = 'Faiza'
+// alert('your name ' + username)
+
+// username='ace'
+// alert('your name'+username)
+//                                                 ch 2
+// var username = "Mark";
+
+// var nationality;
+// nationality = "U.S.";
+// alert('your name and nationality'+"" +username + "" + nationality)
+
+// var floogle = "Mark";
+// alert('his name '+ floogle)
+
+// var lessonAuthor = "Mark";
+// alert('lesson authir name ' + lessonAuthor)
+// var guyWhoKeepsSayingHisOwnName = "Mark";
+// alert('name'+guyWhoKeepsSayingHisOwnName)
+// var x = "Mark";
+// alert('name' +""+x)
+
+//                                             
+// var lastName = "Smith";
+
+// var cityOfOrigin = "New Orleans";
+// var aussieGreeting = "g'Day";
+// alert('your last name , city of origin and aussie greeeting'  +"" + lastName + "" + cityOfOrigin + " " + aussieGreeting)
+
+// var nickname = "Bub";
+// alert('nickname' + "" + nickname)
+// var thanx = "Thanks for your input!"
+//  alert(thanx);
+                        //ch3    
+// var weight = 150;
+// alert(weight+25)
+
+// var originalNum = 23;
+//  var newNum = originalNum + 7;
+// alert(newNum)
+
+
+//  var originalNum = 23;
+// var numToBeAdded = 7;
+//  var newNum = originalNum + numToBeAdded;
+//  alert(newNum)
+
+// var originalNum = 90;
+// originalNum = originalNum + 10;
+// alert(originalNum)
+
+// var originalNum = "23";
+// var newNum = originalNum + 7;
+// alert(newNum)
+
+
+// var prezWhoCame1st = "Washington";
+// alert(prezWhoCame1st)
+
+// var originalNum = 90;
+// originalNum = originalNum + 10;
+// alert(originalNum)
+
+//  var originalNum = "23";
+//  var newNum = originalNum + 7;
+//  alert(newNum)
+
+//  var originalNumber = 23;
+//  var newNumber = originalNumber + 7;
+//  alert(newNumber)
+                            // ch 4
+// userResponse
+// userResponseTime
+// userResponseTimeLimit
+// response
+                                // ch 5
+// var popularNumber = 4;
+// alert(popularNumber)
+
+// var popularNumber = 2 + 2;
+// alert(popularNumber)
+
+// alert(2 + 2);
+
+// var popularNumber = 12 - 24;
+// alert(popularNumber)
+
+// var popularNumber = 3 * 12;
+// alert(popularNumber)
+
+// var num = 10;
+//  var popularNumber = num + 200;
+//  alert(popularNumber)
+
+// var num = 10;
+//  var anotherNum = 1;
+//  var popularNumber = num + anotherNum;
+// alert(popularNumber)
+
+//                         ch 6
+// var num=4;
+// num = num + 1;
+// alert(num)
+
+
+// var num=5
+// num--;
+// alert(num)
+
+// var num = 1;
+// var newNum = ++num;
+// alert(newNum)
+
+// var num = 1;
+// var newNum = num--;
+// alert(newNum)
+
+// var num = 1;
+//  var newNum = --num;
+//  alert(newNum)
+//                                    CH 7
+
+// var totalCost = 1 + (3 * 4);
+// alert(totalCost)
+
+
+// var totalCost = (1 + 3) * 4;
+// alert(totalCost)
+
+// var resultOfComputation = (2 * 4) * 4 + 2;
+// alert(resultOfComputation)
+
+
+// resultOfComputation = ((2 * 4) * 4) + 2
+// alert(resultOfComputation)
+
+//                                    ch 8
+
+
+
+// alert("Thanks for your input!");
+
+// var message = "Thanks for your input!";
+//  alert(message);
+
+// var userName = faiza;
+// alert( 'thanks' + userName + '!')
+
+// var message = "Thanks, ";
+//  var banger = "!";
+// alert(message + userName + banger);
+
+// var message = "Thanks, ";
+// var userName = "Susan";
+//  var banger = "!";
+//  var customMess = message + userName + banger;
+//  alert(customMess);
+
+    //    ch 9
+
+
+    // var spec = prompt("Your species?", "human");
+
+    //  var question = "Your species?";
+    //  var defaultAnswer = "human";
+    //  var spec = prompt(question, defaultAnswer);
+    
+    var numberOfCats = prompt("How many cats?");
+     var tooManyCats = numberOfCats + 1;
+     var tooManyCats= prompt(tooManyCats)
+    
+

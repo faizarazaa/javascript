2bd15516640c014989c2b31b3d98d3f616d51f29
faizarazaa <125462568+faizarazaa@users.noreply.github.com>
2023-04-28 00:22:59 +0500

    Add files via upload

---
+++ javascript/script/assignment1.js
@@ -0,0 +1,114 @@
+// //          TASK 1
+//  var zakatpercentage = 0.025;
+//  var userinput= +prompt( 'enter your value ')
+//   var result =  (zakatpercentage*userinput);
+//   alert ('your zakat value is '+result)
+
+
+//              task2 
+
+// var fitravalue;
+// var familymembers= prompt('enter your family members')
+
+// var wheat=250 ;
+// var dates= 2100;
+// var raisin=2800;
+// var oats= 450;
+// var fitramethod= +prompt('choose the fitra  method  wheat , oats, dates , raisin')
+// if("fitramethod ==wheat")
+// {
+//           fitravalue= (familymembers*wheat)
+
+// }
+// else if("fitramethod == dates")
+// {
+//  fitravalue=(familymembers*dates)
+
+// }
+// else if("fitramethod== raisin")
+// {
+// fitravalue=(familymembers*raisin)
+
+// }
+// else if("fitramethod== oats"){
+// fitravalue  =(familymembers*oats)
+
+// }
+// alert('your fitra amount is '+fitravalue)
+      
+
+
+                                   //   task 3
+
+
+// var secretnum=6;
+//  var num =+prompt("guess the number ")
+//  if(num==6)
+//  {
+//         alert("Congratulations! You guessed the secret number")
+//  }
+// else if(num<6)
+// {
+        
+//     alert("oops! better luck next time")
+         
+// }
+// else if(num>6)
+// {
+//         alert("oops! better luck next time")
+//  }
+
+//    task 4
+
+//  var username = prompt("enter a name ")
+//  var captalized= username.slice(0,1).toUpperCase() + username.slice(1).toLowerCase()
+//  console.log(captalized);
+
+//     task 5
+
+// TASK 7
+
+
+// var nationality = prompt('enter a nationality')
+// var gender = prompt("enter a gender")
+// var age = prompt("enter your age")
+// if(nationality.toLowerCase() == ' Pakistani '  || nationality.toLowerCase() == 'indian' && gender== 'male' && age >18)
+// {
+// alert('you are eligible')
+// }
+// else  if(gender== 'female')
+// {
+//     var mar = prompt('áre you married enter Y OR N?')
+// }
+// else  if(mar == 'Y'&&  nationality.toLowerCase() == ' Pakistani '  || nationality.toLowerCase()== 'indian'  && age >=18  )
+// {
+//     alert('you are eligible')
+// }
+// else if(mar == 'N'&&  nationality.toLowerCase() == ' Pakistani '  || nationality.toLowerCase()== 'indian' && age >=18  )
+// {
+//    alert('you are  not eligible')
+// }
+
+// else{
+//     alert('you are not eligible')
+// }
+
+
+
+
+
+
+
+//  task 8
+
+// var worldcupsquad=['Babar', 'Azam','Iftikhar' ,'Ahmed',
+//     'Haris','Rauf',
+//     'Shaheen','Shah', 'Afridi',
+//     'Saim' ,'Ayub',
+//     'MohammadRizwan',
+//     'NaseemShah']
+
+// console.log(worldcupsquad)
+//  var finalteam= worldcupsquad.slice(0,12);
+//  console.log(finalteam)
+
